c60bfac336cd970402156aaa838ff200fba366a6
pocari <caffelattenonsugar@gmail.com>
2024-12-02 10:28:00 +0900

    修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div>
-      env_var: {process.env.NEXT_PUBLIC_ENV_VAR}
-      test value2
+      <div>env_var: {process.env.NEXT_PUBLIC_ENV_VAR}</div>
+      <div>test value2</div>
     </div>
   );
 }
